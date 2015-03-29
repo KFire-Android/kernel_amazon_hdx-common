dda2b0cef1c603be89d5275dcac317bd8b9fdf40
Hashcode <hashcode0f@gmail.com>
2015-03-29 10:33:07 -0700

    amazon update 1314.4.5.3

--- drivers/usb/misc/mdm_data_bridge.c
+++ drivers/usb/misc/mdm_data_bridge.c
@@ -1084,6 +1084,9 @@ static const struct usb_device_id bridge_ids[] = {
 	{ USB_DEVICE_INTERFACE_NUMBER(0x5c6, 0x9079, 4),
 	.driver_info = (unsigned long)rmnet_hsusb_bridge_names,
 	},
+	{ USB_DEVICE_INTERFACE_NUMBER(0x1949, 0x9005, 3),
+	.driver_info = (unsigned long)rmnet_hsic_bridge_names,
+	},
 
 	{ } /* Terminating entry */
 };
